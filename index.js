4886b05073ab3701145f3a02e50212ec35915cc6
Swapnik Katkoori <katkoor2@msu.edu>
2019-08-25 01:06:48 -0400

    launch app

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 confirmationButton = document.getElementsByClassName("confirm_hyperlink")[0];
 denyButton = document.getElementsByClassName("deny")[0];
-
 confirmationButton.addEventListener("click", confirmClicked);
 denyButton.addEventListener("click", denyClicked);
 
-var socket = io('https://git.heroku.com/quiet-bayou-57031.git');
+var socket = io('https://ancient-crag-62559.herokuapp.com/');
 
 function confirmClicked(){
 	socket.emit('confirm', { confirm: 'Package was confirmed' });
